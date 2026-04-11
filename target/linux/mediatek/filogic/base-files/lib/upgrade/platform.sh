67e275d0b5b33832b760b2bafd1b497445da96dd
Jameslau-tech <Jameslau@126.com>
2026-04-11 10:54:55 +0800

    Update platform.sh

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -135,6 +135,7 @@ platform_do_upgrade() {
 	acer,vero-w6m|\
 	airpi,ap3000m|\
 	arcadyan,mozart|\
+	clx,s20m|\
 	clx,s20l|\
 	clx,s20p|\
 	glinet,gl-mt2500|\
@@ -359,6 +360,7 @@ platform_copy_config() {
 	acer,vero-w6m|\
 	airpi,ap3000m|\
 	arcadyan,mozart|\
+	clx,s20m|\
 	clx,s20l|\
 	clx,s20p|\
 	glinet,gl-mt2500|\
